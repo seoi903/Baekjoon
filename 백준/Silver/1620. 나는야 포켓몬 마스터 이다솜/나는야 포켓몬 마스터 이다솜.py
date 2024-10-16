dd95be28190a0435dc6045a812d8400502634e21
Seoyeong Lee <leeseoi903@gmail.com>
2024-10-17 05:42:26 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 220 ms, Memory: 51260 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, input().split())
+pokedic_int_key = {}
+pokedic_name_key = {}
+for i in range(n):
+    name = sys.stdin.readline().strip()
+    pokedic_int_key[i] = name
+    pokedic_name_key[name] = i
+
+for _ in range(m):
+    item = sys.stdin.readline().strip()
+    if item.isdigit() == True:
+        print(pokedic_int_key[int(item)-1])
+    else:
+        print(pokedic_name_key[item]+1)
