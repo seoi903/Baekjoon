2e5d60c5f35e3af25e25019cbc349f1bbc47fafc
Seoyeong Lee <leeseoi903@gmail.com>
2025-07-13 12:05:56 +0900

    [Bronze II] Title: 가장 쉬운 문제를 찾는 문제, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/22966. 가장 쉬운 문제를 찾는 문제/가장 쉬운 문제를 찾는 문제.py
@@ -0,0 +1,13 @@
+a = int(input())
+title = []
+num = []
+
+for i in range(a):
+    b, c = input().split()
+    title.append(b)
+    num.append(c)
+
+
+for i in range(len(num)):
+    if num[i] == min(num):
+        print(title[i])
